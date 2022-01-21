4efcc9c22efa90801e7bfa5c583c2ab181050ab4
Jeff Beier <jeff.beier@cygrp.com>
2022-01-21 15:48:47 -0600

    initial entry

---
+++ src/main/java/com/example/theme_park_rides_maven/ThemeParkRidesMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.theme_park_rides_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemeParkRidesMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemeParkRidesMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/theme_park_rides_maven/ThemeParkRidesMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.theme_park_rides_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemeParkRidesMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
