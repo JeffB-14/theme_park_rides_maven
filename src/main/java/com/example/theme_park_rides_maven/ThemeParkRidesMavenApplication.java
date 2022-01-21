60b77f8808bf114089956f0bee69f52199c4f5c5
Jeff Beier <jeff.beier@cygrp.com>
2022-01-21 16:04:18 -0600

    base code ready for Jenkins

--- src/main/java/com/example/theme_park_rides_maven/ThemeParkRidesMavenApplication.java
+++ src/main/java/com/example/theme_park_rides_maven/ThemeParkRidesMavenApplication.java
@@ -1,7 +1,11 @@
 package com.example.theme_park_rides_maven;
 
+import com.example.theme_park_rides_maven.entity.ThemeParkRide;
+import com.example.theme_park_rides_maven.repository.ThemeParkRideRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ThemeParkRidesMavenApplication {
@@ -10,4 +14,12 @@ public class ThemeParkRidesMavenApplication {
 		SpringApplication.run(ThemeParkRidesMavenApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner sampleData(ThemeParkRideRepository repository) {
+		return (args) -> {
+			repository.save(new ThemeParkRide("Rollercoaster", "Train ride that speeds you along.", 5, 3));
+			repository.save(new ThemeParkRide("Log flume", "Boat ride with plenty of splashes.", 3, 2));
+			repository.save(new ThemeParkRide("Teacups", "Spinning ride in a giant tea-cup.", 2, 4));
+		};
+	}
 }
---
+++ src/main/java/com/example/theme_park_rides_maven/repository/ThemeParkRideRepository.java
@@ -0,0 +1,12 @@
+package com.example.theme_park_rides_maven.repository;
+
+import com.example.theme_park_rides_maven.entity.ThemeParkRide;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ThemeParkRideRepository extends CrudRepository<ThemeParkRide, Long> {
+    List<ThemeParkRide> findByName(String name);
+}
